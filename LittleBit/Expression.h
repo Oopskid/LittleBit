7a27f4b66b9d11bd22826ed6b3766f7b8609cdf7
Adam <ahughesformal@gmail.com>
2021-09-02 22:41:27 +0100

    Prerequisite expression structure

---
+++ LittleBit/Expression.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "LittleIntern.h"
+#include "LittleCompiler.h"
+namespace LilBit
+{
+	//Container representing a complex operation
+	class Expression
+	{
+		public:
+
+		private:
+		ID destination;
+
+	};
+}
+
